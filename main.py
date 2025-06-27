56d5c8aa62acd8726267cb48a9482513238bd8dc
Kikelomo Abiodun Otutuloro <k.otutuloro@gmail.com>
2025-06-27 21:13:35 +0200

    create additional models

--- main.py
+++ main.py
@@ -4,28 +4,70 @@ from uuid import UUID, uuid4
 from typing import Annotated
 
 
-class User(SQLModel, table=True):
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
+class UserBase(SQLModel):
+    """Shared User props"""
     username: str = Field(unique=True, index=True)
     email: str | None = Field(default=None, unique=True)
-    password_hash: str
     name: str
+
+
+class User(UserBase, table=True):
+    """DB model for user table"""
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    password_hash: str
 
     hobby_links: list["UserHobbyLink"] = Relationship(back_populates="user")
 
 
-class Hobby(SQLModel, table=True):
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
+class UserPublic(UserBase):
+    """Props to return for User"""
+    id: UUID
+
+
+class UserCreate(UserBase):
+    """Props to receive on User creation"""
+    password: str = Field(min_length=8, max_length=40)
+
+
+class UserUpdate(UserBase):
+    """Props to receive on User update"""
+    pass
+
+
+class HobbyBase(SQLModel):
+    """Shared Hobby props"""
     name: str
     description: str | None = None
 
 
+class Hobby(HobbyBase, table=True):
+    """DB model for hobby table"""
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+
+
+class HobbyPublic(HobbyBase):
+    """Props to return for Hobby"""
+    id: UUID
+
+
+class HobbyCreate(HobbyBase):
+    """Props to receive on Hobby creation"""
+    pass
+
+
+class HobbyUpdate(HobbyBase):
+    """Props to receive on Hobby update"""
+    pass
+
+
 class UserHobbyBase(SQLModel):
+    """Shared UserHobby props"""
     interested: bool = True
     rating: int | None = None
 
 
 class UserHobbyLink(UserHobbyBase, table=True):
+    """DB model for userhobbylink table"""
     user_id: UUID = Field(foreign_key="user.id", primary_key=True)
     hobby_id: UUID = Field(foreign_key="hobby.id", primary_key=True)
 
@@ -33,7 +75,25 @@ class UserHobbyLink(UserHobbyBase, table=True):
     hobby: Hobby = Relationship()
 
 
-engine = create_engine("sqlite:///hobby-explorer.db", echo=True)
+class UserHobbyPublic(UserHobbyBase):
+    """Props to return for UserHobby"""
+    user_id: UUID
+    hobby_id: UUID
+
+
+class UserHobbyCreate(UserHobbyBase):
+    """Props to receive on UserHobby creation"""
+    pass
+
+
+class UserHobbyUpdate(UserHobbyBase):
+    """Props to receive on UserHobby update"""
+    pass
+
+
+connect_args = {"check_same_thread": False}
+engine = create_engine("sqlite:///hobby-explorer.db",
+                       echo=True, connect_args=connect_args)
 
 
 def create_db_and_tables():
@@ -56,20 +116,22 @@ def on_startup():
     create_db_and_tables()
 
 
-@app.post("/users")
-def create_user(user: User, session: SessionDep):
-    session.add(user)
+@app.post("/users", response_model=UserPublic)
+def create_user(session: SessionDep, user: UserCreate):
+    db_user = User.model_validate(
+        user, update={"password_hash": user.password})
+    session.add(db_user)
     session.commit()
-    session.refresh(user)
-    return {"user": user, "message": "User created (placeholder)"}
+    session.refresh(db_user)
+    return db_user
 
 
-@app.get("/users/{user_id}")
-def get_user(user_id: UUID, session: SessionDep):
+@app.get("/users/{user_id}", response_model=UserPublic)
+def get_user(session: SessionDep, user_id: UUID):
     user = session.get(User, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    return {"user": user, "message": "Get user (placeholder)"}
+    return user
 
 
 @app.put("/users/{user_id}")
@@ -84,13 +146,13 @@ def delete_user(session: SessionDep, user_id: UUID):
         raise HTTPException(status_code=404, detail="User not found")
     session.delete(user)
     session.commit()
-    return {"user_id": user_id, "message": "Deleted user (placeholder)"}
+    return {"ok": True}
 
 
-@app.get("/users/{user_id}/hobbies")
+@app.get("/users/{user_id}/hobbies", response_model=list[HobbyPublic])
 def get_user_hobbies(session: SessionDep, user_id: UUID, offset: int = 0, limit: Annotated[int, Query(le=100)] = 10):
     hobbies = session.exec(select(Hobby).offset(offset).limit(limit)).all()
-    return {"user_id": user_id, "hobbies": hobbies, "message": "Get user hobbies (placeholder)"}
+    return hobbies
 
 
 @app.post("/users/{user_id}/hobbies")
