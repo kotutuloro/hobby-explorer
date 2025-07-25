c5b3c65477010bb6bd5631961393095381b1eb99
Kikelomo Abiodun Otutuloro <k.otutuloro@gmail.com>
2025-07-25 18:07:21 +0200

    rename test-db service

--- app/config.py
+++ app/config.py
@@ -9,9 +9,10 @@ class Settings(BaseSettings):
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_SERVER: str = "db"
-    POSTGRES_PORT: int = 5432
     POSTGRES_DB: str
+    POSTGRES_TEST_SERVER: str = "test-db"
     POSTGRES_TEST_DB: str = ""
+    POSTGRES_PORT: int = 5432
 
     # SQLALCHEMY_DATABASE_URI
     @computed_field  # type: ignore[prop-decorator]
@@ -33,7 +34,7 @@ class Settings(BaseSettings):
             scheme="postgresql",
             username=self.POSTGRES_USER,
             password=self.POSTGRES_PASSWORD,
-            host=self.POSTGRES_SERVER,
+            host=self.POSTGRES_TEST_SERVER,
             port=self.POSTGRES_PORT,
             path=self.POSTGRES_TEST_DB,
         )
