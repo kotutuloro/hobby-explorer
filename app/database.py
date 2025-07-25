614dadcb4d410e8e59664429346e00372f74b974
Kikelomo Abiodun Otutuloro <k.otutuloro@gmail.com>
2025-07-25 18:24:35 +0200

    add alembic migration service

--- app/database.py
+++ app/database.py
@@ -5,10 +5,6 @@ from .config import settings
 engine = create_engine(str(settings.DATABASE_URL))
 
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
-
 def get_session():
     with Session(engine) as session:
         yield session
