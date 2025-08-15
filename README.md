c9f0cf1ff4761924f63bd68bb8f07b25af0b6f4a
Kikelomo Abiodun Otutuloro <k.otutuloro@gmail.com>
2025-08-15 18:03:13 +0200

    add seed hobby data

--- README.md
+++ README.md
@@ -8,8 +8,6 @@ WIP
 
 ### Todos:
 
-- Postgres
-  - find data to populate db
 - ML Suggestion Service
 - Abstract db actions out from routers
 - User authentication
--- app/db/seed/__main__.py
+++ app/db/seed/__main__.py
@@ -1,16 +1,18 @@
+import os
 
 from app.db.database import get_session
 from .helpers import seed_hobbies_data
 
-
+SEED_FILE_DIR = os.path.dirname(__file__)
 HOBBIES_SEED_FILE = "hobbies.csv"
 
 
 def main():
     print("seeding data")
 
-    session = get_session()
-    with open(HOBBIES_SEED_FILE, "r") as f:
+    session = next(get_session())
+    seed_path = os.path.join(SEED_FILE_DIR, HOBBIES_SEED_FILE)
+    with open(seed_path, "r") as f:
         seed_hobbies_data(f, session)
 
     print("seeding complete")
